e8d1f644e96fc04aa955ca73b6d0965d65c5494c
OliverBentley1 <ob53525@pausd.us>
2024-05-24 09:06:38 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main {
+    public static Frame frame;
+    public static void main(String[] args) {
+        Random r = new Random();
+        //creates the frame
+        frame = new Frame();
+        Line line = new Line();
+    }
+}
