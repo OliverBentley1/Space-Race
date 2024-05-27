f7206622b08e7b4cd58fc8bf10ec4f9a5d2e9ddc
OliverBentley1 <ob53525@pausd.us>
2024-05-26 18:29:45 -0700

    Update Line.java

--- Line.java
+++ Line.java
@@ -5,9 +5,6 @@ public class Line {
     public static int y = 0;
     public Line () {
         Frame.stop = false;
-        for (int i = 0; i < Panel.NUMDOTS; i++) {
-            new Dot(i);
-        }
         Timer timer = new Timer();
         TimerTask tt = new TimerTask() {
     	    @Override
@@ -15,9 +12,9 @@ public class Line {
                 y++;
                 if (y >= 500) {
                     Frame.stop = true;
-                    if (Frame.p1Score > Frame.p2Score) {
+                    if (Main.p1Score > Main.p2Score) {
                         Panel.mainLabel.setText("Left Ship Wins");
-                    } else if (Frame.p2Score > Frame.p1Score) {
+                    } else if (Main.p2Score > Main.p1Score) {
                         Panel.mainLabel.setText("Right Ship Wins");
                     } else {
                         Panel.mainLabel.setText("Tie Game");
